1dd90b9f53e8812efbc6530e54167c5086cb1b98
umangpatel8123 <umangpatel81203@gmail.com>
2023-09-12 13:28:11 +0530

    request api

--- routes/provider.js
+++ routes/provider.js
@@ -1,9 +1,19 @@
 import express from 'express';
-import {createProvider, getProvider} from '../controllers/provider.js';
+import {
+  createProvider,
+  getProvider,
+  acceptRequest,
+  rejectRequest,
+  getRequests,
+} from '../controllers/provider.js';
 
 const router = express.Router();
 
 router.post('/create-provider', createProvider);
 router.post('/get-provider', getProvider);
+
+router.post('/acceptRequest', acceptRequest);
+router.post('/rejectRequest', rejectRequest);
+router.post('/getRequests', getRequests);
 
 export default router;
